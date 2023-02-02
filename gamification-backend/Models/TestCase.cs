bbbdcea20a49f95e5fcad2365dc0771c7e1f2f43
JorBernt <berntsen.jorgen@gmail.com>
2023-02-02 12:40:20 +0100

    Added models for Task, TestCases and results

---
+++ gamification-backend/Models/TestCase.cs
@@ -0,0 +1,13 @@
+﻿namespace GamificationBackend.Models;
+
+public struct TestCase
+{
+    public TestCase(string input, string output)
+    {
+        this.input = input;
+        this.output = output;
+    }
+
+    public string input { get; }
+    public string output { get; }
+}
---
+++ gamification-backend/Models/TestResult.cs
@@ -0,0 +1,8 @@
+﻿namespace GamificationBackend.Models;
+
+public class TestResult
+{
+    public bool Success { get; set; } = false;
+    public bool Error { get; set; } = false;
+    public string? Description { get; set; }
+}
