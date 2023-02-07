2f0d09bf3dd6415b8891e94096afbb5a248515a0
JuanMephisto <adrian@grotter.net>
2023-02-07 09:18:03 +0100

    Improved how rewards are stored and accessed

---
+++ gamification-backend/Models/TaskRewards.cs
@@ -0,0 +1,16 @@
+﻿namespace gamification_backend.Models;
+
+public class TaskRewards
+{
+    public TaskRewards(int lives, int time)
+    {
+        Lives = lives;
+        Time = time;
+    }
+
+    public int Lives { get; set; }
+
+    public int Time { get; set; }
+
+    public int Points { get; set; }
+}
