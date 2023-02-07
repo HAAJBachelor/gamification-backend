f871114d77424d2d8db1509d9acba2db80626207
JorBernt <berntsen.jorgen@gmail.com>
2023-02-07 13:38:29 +0100

    Set up GameTask retrieval from DB

--- gamification-backend/DAL/GameRepository.cs
+++ gamification-backend/DAL/GameRepository.cs
@@ -1,9 +1,17 @@
 ﻿using gamification_backend.Models;
+using gamification_backend.Service;
 
 namespace gamification_backend.DAL;
 
 public class GameRepository : IGameRepository
 {
+    private readonly TasksService _tasksService;
+
+    public GameRepository(TasksService tasksService)
+    {
+        _tasksService = tasksService;
+    }
+
     /*public GameTask GetTask()
     {
         var task = new GameTask();
@@ -14,6 +22,9 @@ public class GameRepository : IGameRepository
 */
     public List<GameTask> GenerateTaskSet()
     {
+        var t = _tasksService.GetAsync();
+        return t.Result;
+
         // Makes 3 GameTask objects and returns a List<GameTask> containing all 3 objects
 
         var taskset = new List<GameTask>();
