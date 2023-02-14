9f1b89fb51df757d421d75ec5de55599a28afeb9
JuanMephisto <adrian@grotter.net>
2023-02-14 10:36:05 +0100

    Created SessionRecord.cs

---
+++ gamification-backend/Models/SessionRecord.cs
@@ -0,0 +1,15 @@
+﻿namespace gamification_backend.Models;
+
+public class SessionRecord
+{
+    public SessionRecord(int id, int score, int time)
+    {
+        Id = id;
+        Score = score;
+        Time = time;
+    }
+
+    public int Id { get; }
+    public int Score { get; }
+    public int Time { get; }
+}
