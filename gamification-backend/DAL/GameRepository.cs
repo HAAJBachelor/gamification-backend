b17398a1f69060f90c6001dfe716490ce70f8b35
jorber <berntsen.jorgen@gmail.com>
2023-02-21 09:28:53 +0100

    Removed output of session timer

--- gamification-backend/DAL/GameRepository.cs
+++ gamification-backend/DAL/GameRepository.cs
@@ -55,6 +55,5 @@ public class GameRepository : IGameRepository
         Console.WriteLine("Saving session from repo");
         await _tasksService.CreateAsync(sessionRecord);
         var s = await _tasksService.GetAsync(sessionRecord.Id);
-        Console.WriteLine(s.Time);
     }
 }
