71ecbb1a2d8734d3f7d9e970f3d05408d4598f67
JuanMephisto <adrian@grotter.net>
2023-03-07 14:51:34 +0100

    User can now save a username after session has ended

--- gamification-backend/DAL/IGameRepository.cs
+++ gamification-backend/DAL/IGameRepository.cs
@@ -9,5 +9,6 @@ namespace gamification_backend.DAL
 
         public Task<List<GameTask>> GenerateTaskSet();
         public Task<bool> SaveSession(SessionRecord sessionRecord);
+        void SaveUsername(int sessionId, string username);
     }
 }
