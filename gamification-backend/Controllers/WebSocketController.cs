7a27cb13358332bb4517526cbb615a36c28cfea7
jorber <berntsen.jorgen@gmail.com>
2023-03-09 11:39:50 +0100

    Removed console log

--- gamification-backend/Controllers/WebSocketController.cs
+++ gamification-backend/Controllers/WebSocketController.cs
@@ -23,41 +23,6 @@ public class WebSocketController : Controller
         }
     }
 
-
-    private class Message
-    {
-        internal enum Type
-        {
-            Update,
-            StateChange,
-        }
-
-        private Message(Type type, string message)
-        {
-            _type = type;
-            data = message;
-        }
-
-        public static Message CreateUpdate(string data)
-        {
-            return new Message(Type.Update, data);
-        }
-
-        public static Message CreateStateChange(string data)
-        {
-            return new Message(Type.StateChange, data);
-        }
-
-        private Type _type;
-
-        public string type
-        {
-            get => _type.ToString();
-        }
-
-        public string data { get; set; }
-    }
-
     private void Echo(WebSocket webSocket)
     {
         var buffer = new byte[1024 * 4];
@@ -90,12 +55,45 @@ public class WebSocketController : Controller
             var json = JsonSerializer.Serialize(data);
             var bytes = Encoding.UTF8.GetBytes(json);
             var size = Encoding.UTF8.GetByteCount(json);
-            Console.WriteLine("Sending: " + json);
-            //Sends time to client
             webSocket.SendAsync(new ArraySegment<byte>(bytes, 0, size),
                 WebSocketMessageType.Text,
                 true,
                 CancellationToken.None);
+        }
+    }
+
+
+    private class Message
+    {
+        private Type _type;
+
+        private Message(Type type, string message)
+        {
+            _type = type;
+            data = message;
+        }
+
+        public string type
+        {
+            get => _type.ToString();
+        }
+
+        public string data { get; set; }
+
+        public static Message CreateUpdate(string data)
+        {
+            return new Message(Type.Update, data);
+        }
+
+        public static Message CreateStateChange(string data)
+        {
+            return new Message(Type.StateChange, data);
+        }
+
+        internal enum Type
+        {
+            Update,
+            StateChange,
         }
     }
 }
