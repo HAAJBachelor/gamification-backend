9b7abc997a31844f253846a1f993025bd907c8bc
JorBernt <berntsen.jorgen@gmail.com>
2023-03-20 12:38:01 +0100

    Fixed bug where a task was successful if you had a compiler error
    It will also send the error message to the frontend

--- gamification-backend/Models/TaskResult.cs
+++ gamification-backend/Models/TaskResult.cs
@@ -2,15 +2,18 @@
 
 public class TaskResult
 {
-    public TaskResult(List<TestCaseResult> testCaseResults, bool success, bool error)
+    public TaskResult(List<TestCaseResult> testCaseResults, bool success, bool error, string compilerErrorMessage = "")
     {
         TestCaseResults = testCaseResults;
         Success = success;
         CompilerError = error;
+        CompilerErrorMessage = compilerErrorMessage;
     }
 
     public bool Success { get; set; }
 
     public bool CompilerError { get; set; }
+
+    public string CompilerErrorMessage { get; set; }
     public List<TestCaseResult> TestCaseResults { get; }
 }
