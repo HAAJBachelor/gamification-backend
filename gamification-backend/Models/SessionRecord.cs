2d2d7bf27bd60e049e3b942e9ba677e895d9be47
JuanMephisto <adrian@grotter.net>
2023-03-27 09:22:49 +0200

    Added DateTime to SessionRecord.cs

--- gamification-backend/Models/SessionRecord.cs
+++ gamification-backend/Models/SessionRecord.cs
@@ -4,11 +4,13 @@ namespace gamification_backend.Models;
 
 public class SessionRecord
 {
-    [Key]
-    public int Id { get; set; }
+    [Key] public int Id { get; set; }
+
     public int Score { get; set; }
     public int Time { get; set; }
-    [Required]
-    public Guid SessionId { get; set; }
+
+    [Required] public Guid SessionId { get; set; }
+
     public string? Username { get; set; }
+    public DateTime TimeCreated { get; set; } = DateTime.Now;
 }
