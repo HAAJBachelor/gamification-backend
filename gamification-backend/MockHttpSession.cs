8adea66806e9da3515f8c69f32f9f3d2cb1e11b3
jorber <berntsen.jorgen@gmail.com>
2023-05-08 11:23:08 +0200

    Added unit tests for GameController

---
+++ gamification-backend/MockHttpSession.cs
@@ -0,0 +1,57 @@
+﻿namespace gamification_backend;
+
+public class MockHttpSession : ISession
+{
+    private readonly Dictionary<string, object> sessionStorage = new();
+
+    public object this[string name]
+    {
+        get => sessionStorage[name];
+        set => sessionStorage[name] = value;
+    }
+
+    void ISession.Set(string key, byte[] value)
+    {
+        sessionStorage[key] = value;
+    }
+
+    bool ISession.TryGetValue(string key, out byte[] value)
+    {
+        if (sessionStorage[key] != null)
+        {
+            value = (byte[]) sessionStorage[key];
+            return true;
+        }
+
+        value = null;
+        return false;
+    }
+
+    // Not nessisary for mocking
+
+    IEnumerable<string> ISession.Keys => throw new NotImplementedException();
+
+    string ISession.Id => throw new NotImplementedException();
+
+    bool ISession.IsAvailable => throw new NotImplementedException();
+
+    void ISession.Clear()
+    {
+        throw new NotImplementedException();
+    }
+
+    void ISession.Remove(string key)
+    {
+        throw new NotImplementedException();
+    }
+
+    Task ISession.CommitAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    Task ISession.LoadAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
--- unit-test/GameController-test.cs
+++ unit-test/GameController-test.cs
@@ -1,35 +1,404 @@
-﻿using gamification_backend.Controllers;
-using gamification_backend.DAL;
+﻿using System.Net;
+using gamification_backend.Controllers;
+using gamification_backend.Models;
 using gamification_backend.Service;
 using Microsoft.AspNetCore.Http;
-using Xunit.Abstractions;
-using Moq;
-using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Sanity.Linq.Extensions;
+using Moq;
 
-namespace unit_test;
-public class GameController_test
+namespace gamification_backend.Tests.Controllers
 {
-    private readonly string _sessionId = "sessionId";
-    private readonly ITestOutputHelper _testOutputHelper;
-    private static readonly Mock<IGameRepository> Repo = new Mock<IGameRepository>();
-    private static readonly Mock<IGameService> Service = new Mock<IGameService>();
-    private readonly GameController _controller = new(Service.Object, new Mock<ILogger<GameController>>().Object);
-
-    // private static readonly Mock<HttpContext> MockHttpContext = new Mock<HttpContext>();
-
-    public GameController_test(ITestOutputHelper testOutputHelper)
+    public class GameControllerTests
     {
-        _testOutputHelper = testOutputHelper;
+        private const string Active = "Active";
+        private const string SessionId = "sessionId";
+        private const string id = "12345678-1234-1234-1234-123456789012";
+        private static readonly Mock<ILogger<GameController>> Mock = new();
+        private static readonly Mock<IGameService> MockGameService = new();
+        private readonly GameController _controller = new(MockGameService.Object, Mock.Object);
+        private readonly Mock<HttpContext> _mockHttpContext = new();
+        private readonly MockHttpSession _mockSession = new();
+
+
+        //test for create session
+        [Fact]
+        public void Create_Session_OK()
+        {
+            //Arrange
+            _mockHttpContext.Setup(s => s.Session).Returns(_mockSession);
+            _controller.ControllerContext.HttpContext = _mockHttpContext.Object;
+            MockGameService.Setup(s => s.CreateSession(It.IsAny<Guid>()));
+            //Act
+            var result = _controller.CreateSession().Result as OkObjectResult;
+            //Assert
+            Assert.Equal("A session was Created", result?.Value);
+            //Assert status code
+            Assert.Equal((int) HttpStatusCode.OK, result?.StatusCode);
+        }
+
+        [Fact]
+        public void Create_Session_Already_Exists_OK()
+        {
+            //Arrange
+            _mockSession.SetString(Active, "Active");
+            _mockSession.SetString(SessionId, id);
+            _mockHttpContext.Setup(s => s.Session).Returns(_mockSession);
+            _controller.ControllerContext.HttpContext = _mockHttpContext.Object;
+            MockGameService.Setup(s => s.CreateSession(It.IsAny<Guid>()));
+            //Act
+            var result = _controller.CreateSession().Result as OkObjectResult;
+            //Assert
+            Assert.Equal("A session was Created", result?.Value);
+            //Assert status code
+            Assert.Equal((int) HttpStatusCode.OK, result?.StatusCode);
+        }
+
+        //test for submit task
+        [Fact]
+        public void Submit_Task_OK()
+        {
+            //Arrange
+            _mockSession.SetString(Active, "Active");
+            _mockSession.SetString(SessionId, id);
+            _mockHttpContext.Setup(s => s.Session).Returns(_mockSession);
+            _controller.ControllerContext.HttpContext = _mockHttpContext.Object;
+            var taskResult = new TaskResult(new List<TestCaseResult>(), true, false);
+            MockGameService.Setup(s => s.SubmitTask(It.IsAny<Guid>(), It.IsAny<string>())).Returns(taskResult);
+            //Act
+            var result = _controller.SubmitTask(It.IsAny<string>()).Result as OkObjectResult;
+            //Assert
+            Assert.Equal((int) HttpStatusCode.OK, result?.StatusCode);
+            Assert.Equal(taskResult, result?.Value);
+        }
+
+        [Fact]
+        public void Submit_Task_Unauthorized()
+        {
+            //Arrange
+            _mockHttpContext.Setup(s => s.Session).Returns(_mockSession);
+            _controller.ControllerContext.HttpContext = _mockHttpContext.Object;
+            MockGameService.Setup(s => s.SubmitTask(It.IsAny<Guid>(), It.IsAny<string>()));
+            //Act
+            var result = _controller.SubmitTask(It.IsAny<string>()).Result as UnauthorizedObjectResult;
+            //Assert
+            Assert.Equal((int) HttpStatusCode.Unauthorized, result?.StatusCode);
+        }
+
+        //test for submit test case
+        [Fact]
+        public void Submit_TestCase_OK()
+        {
+            //Arrange
+            _mockSession.SetString(Active, "Active");
+            _mockSession.SetString(SessionId, id);
+            _mockHttpContext.Setup(s => s.Session).Returns(_mockSession);
+            _controller.ControllerContext.HttpContext = _mockHttpContext.Object;
+            MockGameService.Setup(s => s.SubmitTestCase(It.IsAny<Guid>(), It.IsAny<string>(), It.IsAny<int>()));
+            //Act
+            var result = _controller.SubmitTestCase(It.IsAny<string>(), It.IsAny<int>()).Result as OkObjectResult;
+            //Assert
+            Assert.Equal((int) HttpStatusCode.OK, result?.StatusCode);
+        }
+
+        [Fact]
+        public void Submit_TestCase_Unauthorized()
+        {
+            //Arrange
+            _mockHttpContext.Setup(s => s.Session).Returns(_mockSession);
+            _controller.ControllerContext.HttpContext = _mockHttpContext.Object;
+            MockGameService.Setup(s => s.SubmitTestCase(It.IsAny<Guid>(), It.IsAny<string>(), It.IsAny<int>()));
+            //Act
+            var result =
+                _controller.SubmitTestCase(It.IsAny<string>(), It.IsAny<int>()).Result as UnauthorizedObjectResult;
+            //Assert
+            Assert.Equal((int) HttpStatusCode.Unauthorized, result?.StatusCode);
+        }
+
+        //test for submit test task test case
+        [Fact]
+        public void Submit_Test_Task_TestCase_OK()
+        {
+            //Arrange
+            _mockSession.SetString(Active, "Active");
+            _mockSession.SetString(SessionId, id);
+            _mockHttpContext.Setup(s => s.Session).Returns(_mockSession);
+            _controller.ControllerContext.HttpContext = _mockHttpContext.Object;
+            MockGameService.Setup(s => s.SubmitTestTaskTestCase(It.IsAny<string>(), It.IsAny<int>()));
+            //Act
+            var result =
+                _controller.SubmitTestTaskTestCase(It.IsAny<string>(), It.IsAny<int>()).Result as OkObjectResult;
+            //Assert
+            Assert.Equal((int) HttpStatusCode.OK, result?.StatusCode);
+        }
+
+        // test for select task
+        [Fact]
+        public void Select_Task_OK()
+        {
+            //Arrange
+            _mockSession.SetString(Active, "Active");
+            _mockSession.SetString(SessionId, id);
+            _mockHttpContext.Setup(s => s.Session).Returns(_mockSession);
+            _controller.ControllerContext.HttpContext = _mockHttpContext.Object;
+            MockGameService.Setup(s => s.SelectTask(It.IsAny<Guid>(), It.IsAny<int>()));
+            //Act
+            var result = _controller.SelectTask(It.IsAny<int>()).Result as OkObjectResult;
+            //Assert
+            Assert.Equal((int) HttpStatusCode.OK, result?.StatusCode);
+        }
+
+        [Fact]
+        public void Select_Task_Unauthorized()
+        {
+            //Arrange
+            _mockHttpContext.Setup(s => s.Session).Returns(_mockSession);
+            _controller.ControllerContext.HttpContext = _mockHttpContext.Object;
+            MockGameService.Setup(s => s.SelectTask(It.IsAny<Guid>(), It.IsAny<int>()));
+            //Act
+            var result = _controller.SelectTask(It.IsAny<int>()).Result as UnauthorizedObjectResult;
+            //Assert
+            Assert.Equal((int) HttpStatusCode.Unauthorized, result?.StatusCode);
+        }
+
+        //test for generate task
+        [Fact]
+        public void Generate_Tasks_OK()
+        {
+            //Arrange
+            _mockSession.SetString(Active, "Active");
+            _mockSession.SetString(SessionId, id);
+            _mockHttpContext.Setup(s => s.Session).Returns(_mockSession);
+            _controller.ControllerContext.HttpContext = _mockHttpContext.Object;
+            MockGameService.Setup(s => s.GenerateTaskSet(It.IsAny<Guid>()));
+            //Act
+            var result = _controller.GenerateTasks().Result as OkObjectResult;
+            //Assert
+            Assert.Equal((int) HttpStatusCode.OK, result?.StatusCode);
+        }
+
+        [Fact]
+        public void Generate_Tasks_Unauthorized()
+        {
+            //Arrange
+            _mockHttpContext.Setup(s => s.Session).Returns(_mockSession);
+            _controller.ControllerContext.HttpContext = _mockHttpContext.Object;
+            MockGameService.Setup(s => s.GenerateTaskSet(It.IsAny<Guid>()));
+            //Act
+            var result = _controller.GenerateTasks().Result as UnauthorizedObjectResult;
+            //Assert
+            Assert.Equal((int) HttpStatusCode.Unauthorized, result?.StatusCode);
+        }
+
+        //test for get selected task
+        [Fact]
+        public void Get_Selected_Task_OK()
+        {
+            //Arrange
+            _mockSession.SetString(Active, "Active");
+            _mockSession.SetString(SessionId, id);
+            _mockHttpContext.Setup(s => s.Session).Returns(_mockSession);
+            _controller.ControllerContext.HttpContext = _mockHttpContext.Object;
+            MockGameService.Setup(s => s.GetSelectedTask(It.IsAny<Guid>())).Returns(new GameTask());
+            //Act
+            var result = _controller.GetSelectedTask().Result as OkObjectResult;
+            //Assert
+            Assert.Equal((int) HttpStatusCode.OK, result?.StatusCode);
+        }
+
+        [Fact]
+        public void Get_Selected_Task_Unauthorized()
+        {
+            //Arrange
+            _mockHttpContext.Setup(s => s.Session).Returns(_mockSession);
+            _controller.ControllerContext.HttpContext = _mockHttpContext.Object;
+            MockGameService.Setup(s => s.GetSelectedTask(It.IsAny<Guid>()));
+            //Act
+            var result = _controller.GetSelectedTask().Result as UnauthorizedObjectResult;
+            //Assert
+            Assert.Equal((int) HttpStatusCode.Unauthorized, result?.StatusCode);
+        }
+
+        [Fact]
+        public void Get_Selected_Task_Not_Found()
+        {
+            //Arrange
+            _mockSession.SetString(Active, "Active");
+            _mockSession.SetString(SessionId, id);
+            _mockHttpContext.Setup(s => s.Session).Returns(_mockSession);
+            _controller.ControllerContext.HttpContext = _mockHttpContext.Object;
+            MockGameService.Setup(s => s.GetSelectedTask(It.IsAny<Guid>())).Returns(() => null);
+            //Act
+            var result = _controller.GetSelectedTask().Result as NotFoundObjectResult;
+            //Assert
+            Assert.Equal((int) HttpStatusCode.NotFound, result?.StatusCode);
+        }
+
+        //test for get leaderboard
+        [Fact]
+        public void Get_Leaderboard_OK()
+        {
+            //Arrange
+            _mockSession.SetString(Active, "Active");
+            _mockSession.SetString(SessionId, id);
+            _mockHttpContext.Setup(s => s.Session).Returns(_mockSession);
+            _controller.ControllerContext.HttpContext = _mockHttpContext.Object;
+            MockGameService.Setup(s => s.GetLeaderboard());
+            //Act
+            var result = _controller.GetLeaderboard().Result as OkObjectResult;
+            //Assert
+            Assert.Equal((int) HttpStatusCode.OK, result?.StatusCode);
+        }
+
+        [Fact]
+        public void Get_State_OK()
+        {
+            //Arrange
+            _mockSession.SetString(Active, "Active");
+            _mockSession.SetString(SessionId, id);
+            _mockHttpContext.Setup(s => s.Session).Returns(_mockSession);
+            _controller.ControllerContext.HttpContext = _mockHttpContext.Object;
+            //Act
+            var result = _controller.GetState().Result as OkObjectResult;
+            //Assert
+            Assert.Equal((int) HttpStatusCode.OK, result?.StatusCode);
+        }
+
+        [Fact]
+        public void Get_State_Unauthorized()
+        {
+            //Arrange
+            _mockHttpContext.Setup(s => s.Session).Returns(_mockSession);
+            _controller.ControllerContext.HttpContext = _mockHttpContext.Object;
+            //Act
+            var result = _controller.GetState().Result as UnauthorizedObjectResult;
+            //Assert
+            Assert.Equal((int) HttpStatusCode.Unauthorized, result?.StatusCode);
+        }
+
+        //test getstartcode
+        [Fact]
+        public void Get_Start_Code_OK()
+        {
+            //Arrange
+            _mockSession.SetString(Active, "Active");
+            _mockSession.SetString(SessionId, id);
+            _mockHttpContext.Setup(s => s.Session).Returns(_mockSession);
+            _controller.ControllerContext.HttpContext = _mockHttpContext.Object;
+            //Act
+            var result = _controller.GetStartCode("java", false).Result as OkObjectResult;
+            //Assert
+            Assert.Equal((int) HttpStatusCode.OK, result?.StatusCode);
+        }
+
+        [Fact]
+        public void Get_Start_Code_Unauthorized()
+        {
+            //Arrange
+            _mockHttpContext.Setup(s => s.Session).Returns(_mockSession);
+            _controller.ControllerContext.HttpContext = _mockHttpContext.Object;
+            //Act
+            var result = _controller.GetStartCode("java", false).Result as UnauthorizedObjectResult;
+            //Assert
+            Assert.Equal((int) HttpStatusCode.Unauthorized, result?.StatusCode);
+        }
+
+        [Fact]
+        public void Get_Start_Code_Test_OK()
+        {
+            //Arrange
+            _mockSession.SetString(Active, "Active");
+            _mockSession.SetString(SessionId, id);
+            _mockHttpContext.Setup(s => s.Session).Returns(_mockSession);
+            _controller.ControllerContext.HttpContext = _mockHttpContext.Object;
+            //Act
+            var result = _controller.GetStartCode("java", true).Result as OkObjectResult;
+            //Assert
+            Assert.Equal((int) HttpStatusCode.OK, result?.StatusCode);
+        }
+
+        [Fact]
+        public void Get_Start_Code_Test_Unathorized_OK()
+        {
+            //Arrange
+            _mockHttpContext.Setup(s => s.Session).Returns(_mockSession);
+            _controller.ControllerContext.HttpContext = _mockHttpContext.Object;
+            //Act
+            var result = _controller.GetStartCode("java", true).Result as OkObjectResult;
+            //Assert
+            Assert.Equal((int) HttpStatusCode.OK, result?.StatusCode);
+        }
+
+        [Fact]
+        public void Is_Game_Session_Active_OK()
+        {
+            //Arrange
+            _mockSession.SetString(Active, "Active");
+            _mockSession.SetString(SessionId, id);
+            _mockHttpContext.Setup(s => s.Session).Returns(_mockSession);
+            _controller.ControllerContext.HttpContext = _mockHttpContext.Object;
+            //Act
+            var result = _controller.IsGameSessionActive().Result as OkObjectResult;
+            //Assert
+            Assert.Equal((int) HttpStatusCode.OK, result?.StatusCode);
+        }
+
+        [Fact]
+        public void Is_Game_Session_Active_Unauthorized()
+        {
+            //Arrange
+            _mockHttpContext.Setup(s => s.Session).Returns(_mockSession);
+            _controller.ControllerContext.HttpContext = _mockHttpContext.Object;
+            //Act
+            var result = _controller.IsGameSessionActive().Result as UnauthorizedObjectResult;
+            //Assert
+            Assert.Equal((int) HttpStatusCode.Unauthorized, result?.StatusCode);
+        }
+
+        //test skip task
+        [Fact]
+        public void Skip_Task_OK()
+        {
+            //Arrange
+            _mockSession.SetString(Active, "Active");
+            _mockSession.SetString(SessionId, id);
+            _mockHttpContext.Setup(s => s.Session).Returns(_mockSession);
+            MockGameService.Setup(s => s.UseSkip(It.IsAny<Guid>()));
+            _controller.ControllerContext.HttpContext = _mockHttpContext.Object;
+            //Act
+            var result = _controller.SkipTask().Result as OkObjectResult;
+            //Assert
+            Assert.Equal((int) HttpStatusCode.OK, result?.StatusCode);
+        }
+
+        [Fact]
+        public void Skip_Task_Unauthorized()
+        {
+            //Arrange
+            _mockHttpContext.Setup(s => s.Session).Returns(_mockSession);
+            _controller.ControllerContext.HttpContext = _mockHttpContext.Object;
+            MockGameService.Setup(s => s.UseSkip(It.IsAny<Guid>()));
+            //Act
+            var result = _controller.SkipTask().Result as UnauthorizedObjectResult;
+            //Assert
+            Assert.Equal((int) HttpStatusCode.Unauthorized, result?.StatusCode);
+        }
+
+        //test submit username
+        [Fact]
+        public void Submit_Username_OK()
+        {
+            //Arrange
+            _mockSession.SetString(Active, "Active");
+            _mockSession.SetString(SessionId, id);
+            _mockHttpContext.Setup(s => s.Session).Returns(_mockSession);
+            MockGameService.Setup(s => s.SaveUsername(It.IsAny<Guid>(), It.IsAny<string>()));
+            _controller.ControllerContext.HttpContext = _mockHttpContext.Object;
+            //Act
+            var result = _controller.SubmitUsername("test").Result as OkObjectResult;
+            //Assert
+            Assert.Equal((int) HttpStatusCode.OK, result?.StatusCode);
+            Assert.Equal("", _mockSession.GetString(Active));
+        }
     }
-
-    /*[Fact]
-    public async Task CreateSession_Ok()
-    {
-        var result = Service.Object.CreateSession();
-        _testOutputHelper.WriteLine("Print: " + result);
-        Assert.Equal(0, result);
-    }*/
 }
